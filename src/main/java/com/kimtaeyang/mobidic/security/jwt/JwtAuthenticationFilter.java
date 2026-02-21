b98dd0e3fc92074d48e9a32e03241a5c71eff1fc
KTH <pni2396@gmail.com>
2026-02-21 19:06:40 +0900

    fix: jwt filter chain 예외 처리 로직 추가
    - 필터체인 내에서의 AuthenticationException에 대한 예외처리 추가

--- src/main/java/com/kimtaeyang/mobidic/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/kimtaeyang/mobidic/security/jwt/JwtAuthenticationFilter.java
@@ -7,7 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,19 +26,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
     private final JwtBlacklistService jwtBlacklistService;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //JWT 파싱
         String jwt = resolveToken(request);
 
-        if (StringUtils.hasText(jwt)
-                && jwtProvider.validateToken(jwt)
-                && !jwtBlacklistService.isTokenWithdrawn(jwt)
-                && !jwtBlacklistService.isTokenLogout(jwt)
-        ) {
-            Authentication authentication = jwtProvider.getAuthentication(jwt);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            if (StringUtils.hasText(jwt)
+                    && jwtProvider.validateToken(jwt)
+                    && !jwtBlacklistService.isTokenWithdrawn(jwt)
+                    && !jwtBlacklistService.isTokenLogout(jwt)
+            ) {
+                Authentication authentication = jwtProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (AuthenticationException e) {
+            SecurityContextHolder.clearContext();
+            authenticationEntryPoint.commence(request, response, e);
+            return;
         }
 
         filterChain.doFilter(request, response);
