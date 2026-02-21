6a880ad96092c7021a6de96f9966b80b3cdba391
KTH <pni2396@gmail.com>
2026-02-21 19:44:16 +0900

    fix: createdAt auditing 추가

--- src/main/java/com/kimtaeyang/mobidic/dictionary/entity/Vocabulary.java
+++ src/main/java/com/kimtaeyang/mobidic/dictionary/entity/Vocabulary.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -33,5 +34,6 @@ public class Vocabulary {
     @Column(name = "description")
     private String description;
     @Column(name = "created_at")
+    @CreatedDate
     private LocalDateTime createdAt;
 }
