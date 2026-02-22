36a4ea1ac88205329d931676a948b28e6254b26d
KTH <pni2396@gmail.com>
2026-02-22 19:58:57 +0900

    chore: unused import 제거

--- src/main/java/com/kimtaeyang/mobidic/user/entity/User.java
+++ src/main/java/com/kimtaeyang/mobidic/user/entity/User.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
