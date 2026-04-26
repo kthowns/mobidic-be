8101dae8c9942085bc62ace4b58a9aaf3063d0c1
KTH <pni2396@gmail.com>
2026-04-26 18:24:39 +0900

    chore: 회원가입 validation 메세지 추가

--- src/main/java/com/kimtaeyang/mobidic/user/dto/SignUpRequestDto.java
+++ src/main/java/com/kimtaeyang/mobidic/user/dto/SignUpRequestDto.java
@@ -13,7 +13,10 @@ import java.util.List;
 @ToString
 public class SignUpRequestDto {
     @NotBlank(message = "이메일을 입력해주세요")
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,100}$", message = "유효하지 않은 이메일 형식입니다.")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,100}$",
+            message = "유효하지 않은 이메일 형식입니다."
+    )
     private String email;
 
     @NotBlank(message = "닉네임을 입력해주세요.")
