ea170ed926ae767a87a476b03b082ec24f2a30d4
KTH <pni2396@gmail.com>
2026-04-26 18:20:41 +0900

    chore: 회원가입 validation 메세지 추가

--- src/main/java/com/kimtaeyang/mobidic/user/dto/SignUpRequestDto.java
+++ src/main/java/com/kimtaeyang/mobidic/user/dto/SignUpRequestDto.java
@@ -12,16 +12,22 @@ import java.util.List;
 @Builder
 @ToString
 public class SignUpRequestDto {
-    @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,100}$", message = "Invalid email pattern")
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,100}$", message = "유효하지 않은 이메일 형식입니다.")
     private String email;
 
-    @NotBlank
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,16}$", message = "Invalid nickname pattern")
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Pattern(
+            regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,16}$",
+            message = "닉네임은 2~16자의 한글, 영문 소문자, 숫자, -, _ 만 사용할 수 있습니다. "
+    )
     private String nickname;
 
-    @NotBlank
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,128}$", message = "Invalid password pattern")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,128}$",
+            message = "비밀번호는 8~128자이며 영문자, 숫자, 특수문자(@$!%*?&)를 각각 1개 이상 포함해야 합니다."
+    )
     //최소 8자, 숫자와 알파벳
     private String password;
 
